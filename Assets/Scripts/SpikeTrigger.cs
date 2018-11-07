05e3679492fed8367b08ef86755962f993d96868
miltoncandelero <miltoncandelero@gmail.com>
2018-11-07 01:15:29 -0300

    Bounce back to spawn on spike trigger enter
    (fixed spawnpoint. just for testing and stuff)

---
+++ Assets/Scripts/SpikeTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform spawn;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag("Spike")) transform.position = spawn.position;
+	}
+}
